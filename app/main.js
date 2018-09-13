166aa0c605b22d4b17943a01b5ac608662e907f3
Mike Crowl <crowlmj@gmail.com>
2018-09-13 01:13:16 -0600

    Refactor app setup, add bunyan logging

--- app/main.js
+++ app/main.js
@@ -1,63 +1,86 @@
 'use strict';
 
 const app = require('express')();
+const bunyan = require('bunyan');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
 const userController = require('./controllers/randomUser');
 const chatAppController = require('./controllers/chatApp');
 
-const APP_PORT = process.env.PORT || 3000;
 
+let logger = setupLogger();
 
-app.use((req, res, next) => {
-  res.io = io;
-  next();
-});
+function setupLogger() {
+  return bunyan.createLogger({name: 'express-messenger-demo'});
+}
 
-//default page
-app.get('/', (req, res) => {
+function setupMiddleware (app) {
 
-  userController.getRandomUser(req, res);
-
-});
-
-app.get('/chat/:user', (req, res) => {
-
-  res.io.emit('message', `User ${req.params.user} has joined`);
-
-  chatAppController.getChatApp(req, res);
-
-});
-
-// healthcheck
-app.get('/status', (req, res) => {
-
-  return res.send('YESOK').status(200);
-
-});
-
-io.on('connection', (socket) => {
-  console.log(`User ${socket.id} connected.`);
-  socket.on('disconnect', () => {
-    console.log(`User ${socket.id} disconnected.`);
-  });
-
-  socket.on('message', (message) => {
-    console.log(`message: ${message}`);
-    io.emit('message', message);
-  });
-
-});
-
-
-//start server
-function startServer(port) {
-
-  http.listen(port, () => {
-    console.log(`App started on localhost:${port}`);
+  app.use((req, res, next) => {
+    res.io = io;
+    next();
   });
 
 }
 
-startServer(APP_PORT);
+function setupRoutes(app) {
+
+  //default page - assigns a username and redirects to chat app
+  app.get('/', (req, res) => {
+
+    userController.getRandomUser(req, res);
+
+  });
+
+  //chat app with username
+  app.get('/chat/:user', (req, res) => {
+
+    res.io.emit('message', `User ${req.params.user} has joined`);
+
+    chatAppController.getChatApp(req, res);
+
+  });
+
+  // healthcheck
+  app.get('/status', (req, res) => {
+
+    return res.send('YESOK').status(200);
+
+  });
+
+}
+
+function setupSocketIO(io) {
+
+  io.on('connection', (socket) => {
+    logger.info(`User ${socket.id} connected.`); // eslint-disable-line
+    socket.on('disconnect', () => {
+      logger.info(`User ${socket.id} disconnected.`); // eslint-disable-line
+    });
+
+    socket.on('message', (message) => {
+      logger.info(`message: ${message}`); // eslint-disable-line
+      io.emit('message', message);
+    });
+
+  });
+
+}
+
+//start server
+function startServer(port) {
+
+  setupMiddleware(app);
+  setupRoutes(app);
+  setupSocketIO(io);
+
+  http.listen(port, () => {
+    logger.info(`App started on localhost:${port}`); // eslint-disable-line
+  });
+
+}
+
+module.exports = {
+  startServer
+};
+
